3ea5fc45cd54442082c850d4cc1189593f78943c
Warren Milburn <warren@munki.net>
2020-05-27 16:23:42 +0100

    Changed to specify dir via cmd line.

--- run_fileserver.bat
+++ run_fileserver.bat
@@ -1,2 +1,1 @@
-cd c:\users\warren\ideaprojects\jbotnet\jbotnetcommon\serve
-python -m http.server 8000 --bind 127.0.0.1
+python -m http.server 8000 --bind 127.0.0.1 --directory c:\users\warren\ideaprojects\jbotnet\jbotnetcommon\serve
